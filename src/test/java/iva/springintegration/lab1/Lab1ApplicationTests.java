41fd90c49e1c9857090f13a64c7094e70888c525
Igor Varshavsky <igor.varshavsky@gmail.com>
2021-02-21 23:53:34 -0500

    Integration stdIn to 2 stdOut adapters

---
+++ src/test/java/iva/springintegration/lab1/Lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package iva.springintegration.lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
